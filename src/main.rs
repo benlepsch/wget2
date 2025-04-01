d467f42d2b8dcbb8c1af91612b20f754691939f2
Ben Lepsch <blepsch@gmail.com>
2025-03-31 22:15:32 -0400

    version 1 working as intended

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use std::env;
+use std::process::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    dbg!(&args);
 
-    println!("Hello, world!");
+    if args.len() < 2 {
+        println!("requires url argument bozo");
+        return
+    }
+
+    println!("{}", &args[1]);
+
+    let resp = Command::new("wget")
+                .arg(args[1].clone())
+                .output()
+                .expect("wget command failed to start");
+
+    println!("status: {}", &resp.status);
+    println!("stdout: {}",  String::from_utf8_lossy(&resp.stdout));
+    println!("stderr: {}",  String::from_utf8_lossy(&resp.stderr));
 }
