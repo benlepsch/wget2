0aa10faf0fd5530f8f5c7d44d5f944a67702e9c4
Ben Lepsch <blepsch@gmail.com>
2025-04-15 13:37:23 +0000

    begin rewrite with http get request instead of reqwest library

--- src/with_reqwest.rs
+++ src/with_reqwest.rs
@@ -1,6 +1,19 @@
 use std::env;
+// use std::process::Command;
+use reqwest;
 use std::fs;
 use std::io::{stdin, Write};
+
+// fn cheater(path: &str) {
+//     let resp = Command::new("wget")
+//                 .arg(path)
+//                 .output()
+//                 .expect("wget command failed to start");
+
+//     println!("status: {}", &resp.status);
+//     println!("stdout: {}",  String::from_utf8_lossy(&resp.stdout));
+//     println!("stderr: {}",  String::from_utf8_lossy(&resp.stderr));
+// }
 
 fn path_exists(path: &str) -> bool {
     fs::metadata(path).is_ok()
@@ -8,6 +21,7 @@ fn path_exists(path: &str) -> bool {
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
+    // dbg!(&args);
 
     if args.len() < 2 {
         println!("no url in arguments, please enter the url to fetch:");
@@ -51,7 +65,21 @@ fn main() {
         filename = "index.html".to_string();
     }
 
-   
+    // println!("{}", &args[1]);
+
+    // wget 2 (version 1)
+    // cheats and just runs the wget command from terminal
+    // cheater(&args[1]);
+
+    println!("connecting to {}", &args[1]);
+    let webpage = reqwest::blocking::get(&args[1])
+                .expect("is this the error message");
+
+    // dbg!(&webpage);
+    
+    let body = webpage.text()
+                .expect("if this fails im fucked");
+    
     // check if filename exists
     // i can feel the type errors already
     println!("using filename {}", &filename);
@@ -74,6 +102,6 @@ fn main() {
     let mut write_out = fs::File::create(filename)
                                 .expect("cmon now");
 
-    // write_out.write_all(&body.as_bytes())
-    //            .expect("sure hope that write worked");
+    write_out.write_all(&body.as_bytes())
+                .expect("sure hope that write worked");
 }
